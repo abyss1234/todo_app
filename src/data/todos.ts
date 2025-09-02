37c6c00b70ca29f97a25599379a81a3a203ade2e
abyss1234 <nboonbin@gmail.com>
2025-09-02 13:20:39 +0800

    feat: initial Vite React TS app

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "hi",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "hi bitch",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "hi boy",
+        completed: false,
+    },
+];
